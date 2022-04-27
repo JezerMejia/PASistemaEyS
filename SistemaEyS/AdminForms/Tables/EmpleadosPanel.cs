04e4e0cc38fd17c0b1ab5126b0df9f6396dc6c72
Jezer Mejía <amyuki4@icloud.com>
2022-04-27 00:15:31 -0600

    Remove unnecessary code

--- SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
+++ SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
@@ -27,26 +27,6 @@ namespace SistemaEyS.AdminForms.Tables
             {
                 this.treeView.AppendColumn(titulos[i], new CellRendererText(), "text", i);
             }
-
-            {
-            }
-
-            /*StoreObject[] storeObjects = {
-                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Apellido", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Dirección", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Teléfono", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Email", typeof(string), "text", new Gtk.CellRendererText()),
-            };
-
-            this.SetTreeViewColumns(this.treeView, storeObjects);
-
-            this.listStore = (Gtk.ListStore) this.treeView.Model;
-
-            this.listStore.AppendValues("000031725", "Jezer", "Mejía", "", "81211855", "jezer.mejia13523@est.uca.edu.ni");
-            this.listStore.AppendValues("000031231", "Juan", "Juan", "", "81921935", "juan.juan23214@est.uca.edu.ni");
-        */
         }
 
         private void SetTreeViewColumns(Gtk.TreeView treeView, StoreObject[] storeObject)
