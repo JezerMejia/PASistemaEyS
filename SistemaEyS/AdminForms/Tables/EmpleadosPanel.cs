c3dd6492ad3eb17d1215d91a5f0995ae28af474d
LabJ5 <jezer.mejia13523@est.uca.edu.ni>
2022-04-27 17:42:17 -0600

    Use PanelTemplate for EmpleadosPanel

--- SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
+++ SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
@@ -1,50 +1,29 @@
 ﻿using System;
-using System.Data;
-using Mono.Data.Sqlite;
-using MySql.Data.MySqlClient;
 using Gtk;
-using MySql.Data;
-using System.Text;
-using System.Collections.Generic;
 using SistemaEyS.Datos;
 
 namespace SistemaEyS.AdminForms.Tables
 {
     [System.ComponentModel.ToolboxItem(true)]
 
-    public partial class EmpleadosPanel : Gtk.Bin
+    public partial class EmpleadosPanel :PanelTemplate
     {
         Dt_tlb_user dtus = new Dt_tlb_user();
 
         public EmpleadosPanel()
-
         {
-            this.Build();
+            StoreObject[] storeObjects = {
+                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("User", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Apellido", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Dirección", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Teléfono", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Email", typeof(string), "text", new Gtk.CellRendererText()),
+            };
+            this.SetTreeViewColumns(treeView, storeObjects);
+
             this.treeView.Model = dtus.listarUsuarios();
-
-            string[] titulos = { "idUser", "User", "Nombres", "Apellidos", "Email", "Estado" };
-            for (int i = 0; i < titulos.Length; i++)
-            {
-                this.treeView.AppendColumn(titulos[i], new CellRendererText(), "text", i);
-            }
-        }
-
-        private void SetTreeViewColumns(Gtk.TreeView treeView, StoreObject[] storeObject)
-        {
-            Type[] types = new Type[storeObject.Length];
-            for (int i = 0; i < types.Length; i++)
-            {
-                types[i] = storeObject[i].type;
-            }
-            Gtk.ListStore store = new Gtk.ListStore(types);
-            treeView.Model = store;
-            treeView.ShowAll();
-
-            for (int i = 0; i < types.Length; i++)
-            {
-                StoreObject obj = storeObject[i];
-                treeView.AppendColumn(obj.name, obj.cellRenderer, obj.col_type, i);
-            }
         }
     }
 }
