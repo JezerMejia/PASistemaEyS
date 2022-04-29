9276874f9141b61c9c14045860b8824e2a10e590
KaiserStuka <leocnavarrete3@gmail.com>
2022-04-29 01:02:15 -0600

    Added a Tables EntradaSalida and SolicitudVacaciones

---
+++ SistemaEyS/AdminForms/Tables/EntradaSalida.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using SistemaEyS.Datos;
+
+namespace SistemaEyS.AdminForms.Tables
+{
+    [System.ComponentModel.ToolboxItem(true)]
+
+    public partial class entradaSalidaPanel : PanelTemplate
+    {
+        Dt_tlb_entradaSalida dtus = new Dt_tlb_entradaSalida();
+
+        public entradaSalidaPanel()
+        {
+            StoreObject[] storeObjects = {
+                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("User", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Apellido", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Dirección", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Teléfono", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Email", typeof(string), "text", new Gtk.CellRendererText()),
+            };
+            this.SetTreeViewColumns(treeView, storeObjects);
+
+            this.treeView.Model = dtus.listarentradaSalida();
+        }
+    }
+}
---
+++ SistemaEyS/AdminForms/Tables/SolicitudVacaciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using SistemaEyS.Datos;
+
+namespace SistemaEyS.AdminForms.Tables
+{
+    [System.ComponentModel.ToolboxItem(true)]
+
+    public partial class solicitudVacacionesPanel : PanelTemplate
+    {
+        Dt_tlb_solicitudVacaciones dtus = new Dt_tlb_solicitudVacaciones();
+
+        public solicitudVacacionesPanel()
+        {
+            StoreObject[] storeObjects = {
+                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("User", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Apellido", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Dirección", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Teléfono", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Email", typeof(string), "text", new Gtk.CellRendererText()),
+            };
+            this.SetTreeViewColumns(treeView, storeObjects);
+
+            this.treeView.Model = dtus.listarsolicitudVacaciones();
+        }
+    }
+}
