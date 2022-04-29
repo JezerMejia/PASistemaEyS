5014ae3eae3d8707ad0b4f0b037edad454b5f352
KaiserStuka <leocnavarrete3@gmail.com>
2022-04-29 08:58:51 -0600

    Fixed a error in HorarioSetting, because I'm stupid

---
+++ SistemaEyS/AdminForms/Settings/HorarioSenttings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SistemaEyS.AdminForms.Settings
+{
+    public partial class HorarioSenttings : Gtk.Window
+    {
+        public HorarioSenttings() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
