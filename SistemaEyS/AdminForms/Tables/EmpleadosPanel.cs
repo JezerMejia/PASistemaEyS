4c072052f7cf61ffc4cd230b69f8413f094da2bc
Jezer Mejía <amyuki4@icloud.com>
2022-04-29 19:32:17 -0600

    Changed EmpleadosPanel TreeView to show the correct columns

--- SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
+++ SistemaEyS/AdminForms/Tables/EmpleadosPanel.cs
@@ -8,21 +8,22 @@ namespace SistemaEyS.AdminForms.Tables
 
     public partial class EmpleadosPanel :PanelTemplate
     {
-        Dt_tlb_user dtus = new Dt_tlb_user();
-
         public EmpleadosPanel()
         {
             StoreObject[] storeObjects = {
                 new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("User", typeof(string), "text", new Gtk.CellRendererText()),
                 new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
                 new StoreObject("Apellido", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Dirección", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Teléfono", typeof(string), "text", new Gtk.CellRendererText()),
-                new StoreObject("Email", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Ingreso", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Cédula", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Cargo", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Departamento", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Horario", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Grupo", typeof(string), "text", new Gtk.CellRendererText()),
             };
             this.SetTreeViewColumns(treeView, storeObjects);
 
+            Dt_tlb_empleado dtus = new Dt_tlb_empleado();
             this.treeView.Model = dtus.listarUsuarios();
         }
     }
