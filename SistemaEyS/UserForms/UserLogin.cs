da2749f55f918d93d6427bfd3db873186f0b94ea
Jezer Mejía <amyuki4@icloud.com>
2022-04-30 00:33:21 -0600

    Check idEmpleado and password to log in

--- SistemaEyS/UserForms/UserLogin.cs
+++ SistemaEyS/UserForms/UserLogin.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Data;
+using System.Text;
 using Gtk;
+using SistemaEyS.Database.Connection;
+
 namespace SistemaEyS.UserForms
 {
     public partial class UserLogin : Gtk.Window
     {
         protected Window parent;
+        ConnectionEyS conn = ConnectionEyS.OpenConnection();
+
         public UserLogin(Window parent) :
                 base(Gtk.WindowType.Toplevel)
         {
@@ -20,6 +26,44 @@ namespace SistemaEyS.UserForms
             this.parent.Show();
         }
 
+        private bool LogIn(string user, string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            IDataReader idr = null;
+            bool value = false;
+
+            sb.Clear();
+            sb.Append($"SELECT idEmpleado, password FROM BDSistemaEyS.Empleado WHERE idEmpleado = {user};");
+            try
+            {
+                idr = conn.Read(CommandType.Text, sb.ToString());
+
+                if (!idr.Read()) throw new Exception("Empleado no encontrado");
+                if (idr[1].ToString() == password)
+                {
+                    value = true;
+                } else
+                {
+                    throw new Exception("Contraseña incorrecta");
+                }
+            }
+            catch (Exception e)
+            {
+                MessageDialog ms = new MessageDialog(null, DialogFlags.Modal, MessageType.Error,
+                    ButtonsType.Ok, e.Message);
+                ms.Run();
+                ms.Destroy();
+            }
+            finally
+            {
+                if (idr != null && !idr.IsClosed)
+                {
+                    idr.Close();
+                }
+            }
+            return value;
+        }
+
         protected void OnDeleteEvent(object o, DeleteEventArgs args)
         {
             this.Close();
@@ -27,6 +71,14 @@ namespace SistemaEyS.UserForms
 
         protected void btnEnterOnClicked(object sender, EventArgs e)
         {
+            string idEmpleado = this.entUser.Text;
+            string pwdEmpleado = this.entPassword.Text;
+
+            if (!LogIn(idEmpleado, pwdEmpleado))
+            {
+                return;
+            }
+
             UserAssistanceForm assistanceForm = new UserAssistanceForm(this);
             assistanceForm.Show();
             this.Hide();
