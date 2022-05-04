2bdab8ed7cf43f53239ca3e6307ba3a0bef29b32
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-04 02:00:42 -0600

    Added a buttoms from a Table EmpleadosPanel

---
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/AddBtn.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
+{
+    public partial class AddBtn : Gtk.Window
+    {
+        public AddBtn() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+
+        protected void OnButton4Clicked(object sender, EventArgs e)
+        {
+            MessageDialog mensaje = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Ha cancelado");
+            mensaje.Run();
+            mensaje.Destroy();
+            this.Hide();
+
+        }
+    }
+}
---
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/ValidBtn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
+{
+    public partial class ValidBtn : Gtk.Window
+    {
+        public ValidBtn() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
