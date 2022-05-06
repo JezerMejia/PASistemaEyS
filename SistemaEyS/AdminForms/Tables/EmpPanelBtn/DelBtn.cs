6d4323d015a27a4c6f7d193493992f5084427c01
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-06 09:27:01 -0600

    Added a DelBtn option

---
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/DelBtn.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Gtk;
+using SistemaEyS.DatosEyS;
+namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
+{
+    public partial class DelBtn : Gtk.Window
+    {
+
+        Dt_tlb_user dtus = new Dt_tlb_user();
+
+        public DelBtn() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+            llenarcbxUser();
+            this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
+            {
+                args.RetVal = this.HideOnDelete();
+            };
+        }
+
+
+        protected void llenarcbxUser()
+        {
+            ListStore datos = new ListStore(typeof(String), typeof(String));
+            datos = dtus.cbxEUsers();
+            TreeIter iter;
+
+            if (datos.GetIterFirst(out iter))
+            {
+                do
+                {
+                    this.comboboxentry3.InsertText(Convert.ToInt32(datos.GetValue(iter, 0)), (String)datos.GetValue(iter, 1));
+
+                }
+                while (datos.IterNext(ref iter));
+
+            }
+
+            comboboxentry3.Entry.Completion = new EntryCompletion();
+            comboboxentry3.Entry.Completion.Model = datos;
+            comboboxentry3.Entry.Completion.TextColumn = 1;
+        }
+
+
+    }
+
+
+}
