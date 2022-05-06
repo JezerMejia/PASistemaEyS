a183e46df7c3cd0858bbc83214c8bc92612ff7eb
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-06 09:26:25 -0600

    Added a ActBtn option

---
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/ActBtn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gtk;
+
+namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
+{
+    public partial class ActBtn : Gtk.Window
+    {
+        public ActBtn() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+            this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
+            {
+                args.RetVal = this.HideOnDelete();
+            };
+        }
+    }
+}
