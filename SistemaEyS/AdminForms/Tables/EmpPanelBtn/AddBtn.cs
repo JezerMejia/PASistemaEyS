a9750e95d81357d26da7d0a55b5e8fcd963efbe6
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-06 08:47:10 -0600

    Added a insert users in Seguridad.tlb_user

--- SistemaEyS/AdminForms/Tables/EmpPanelBtn/AddBtn.cs
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/AddBtn.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Gtk;
+using SistemaEyS.Database.Connection;
+using MySql.Data.MySqlClient;
+using System.Data;
+using SistemaEySLibrary;
+
 namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
 {
     public partial class AddBtn : Gtk.Window
@@ -8,6 +13,9 @@ namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
                 base(Gtk.WindowType.Toplevel)
         {
             this.Build();
+            Random r = new Random();
+            entry1.Text = Convert.ToString(r.Next(1000, 10000));
+
             this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
             {
                 args.RetVal = this.HideOnDelete();
@@ -19,6 +27,26 @@ namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
             MessageDialog mensaje = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Ha cancelado");
             mensaje.Run();
             mensaje.Destroy();
+        }
+
+        protected void OnButton2Clicked(object sender, EventArgs e)
+        {
+
+            ConnectionSeg conecction = ConnectionSeg.OpenConnection();
+
+            String Query = "INSERT INTO tbl_user (id_user, user, pwd, nombres, apellidos, email, pwd_temp, estado) VALUES ('" + entry1.Text + "','" + entry2.Text + "' , '"
+                            + entry3.Text + "' , '" + entry4.Text + "' , '" + entry5.Text + "' , '" + entry6.Text + "' , '" + entry7.Text + "' , '" + entry8.Text + "');";
+
+            MySqlCommand command = new MySqlCommand(Query, conecction.conn);
+            conecction.Execute(CommandType.Text,Query);
+          
+            ConnectionSeg.CloseConnection();
+
+            MessageDialog mensaje = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Agregado");
+            mensaje.Run();
+            mensaje.Destroy();
+
+           
         }
     }
 }
