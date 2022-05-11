fab9700595698a73aca687a6e3997216f43dabfa
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-10 23:14:59 -0600

    Added a AddDialogHor to Horarioview

---
+++ SistemaEyS/AdminForms/Tables/HorPanelBtn/AddDialogHor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SistemaEyS.AdminForms.Tables.HorPanelBtn
+{
+    public partial class AddDialogHor : Gtk.Window
+    {
+        public AddDialogHor() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+
+        protected void OnButton15Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
