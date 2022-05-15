52c970617e7c85f9e1b21df217d98280a9b31705
Jezer Mejía <amyuki4@icloud.com>
2022-05-15 11:17:52 -0600

    Added RolSeguridad window with its proper functionality

---
+++ SistemaEyS/AdminForms/Seguridad/RolSeguridad.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Gtk;
+using SistemaEyS.DatosSeguridad;
+
+namespace SistemaEyS.AdminForms.Seguridad
+{
+    public partial class RolSeguridad : Gtk.Window
+    {
+        protected Dt_tbl_rol DtRol = new Dt_tbl_rol();
+        protected TreeModelFilter TreeData;
+        protected TreeModelFilterVisibleFunc ModelFilterFunc;
+        public int SelectedID = -1;
+
+        public RolSeguridad() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+            this.Hide();
+
+            this.ModelFilterFunc = new TreeModelFilterVisibleFunc(this.TreeModelFilterVisible);
+
+            this.viewTable.SearchEntry = this.TxtSearch;
+            this.viewTable.SearchEqualFunc = new TreeViewSearchEqualFunc(this.ViewTableEqualFunc);
+
+            this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
+            {
+                args.RetVal = this.HideOnDelete();
+            };
+
+            StoreObject[] storeObjects = {
+                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Nombre", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Descripción", typeof(string), "text", new Gtk.CellRendererText()),
+            };
+            this.viewTable.SetTreeViewColumns(storeObjects);
+
+            this.UpdateData();
+        }
+
+        public void UpdateData()
+        {
+            this.TreeData = new TreeModelFilter(DtRol.GetData(), null);
+            this.TreeData.VisibleFunc = this.ModelFilterFunc;
+            this.viewTable.Model = this.TreeData;
+            //this.FillComboboxModel();
+        }
+
+        protected void BtnNewOnClicked(object sender, EventArgs e)
+        {
+            this.SelectedID = -1;
+            this.TxtName.Text = "";
+            this.TxtEstado.Text = "";
+        }
+
+        protected void BtnAddOnClicked(object sender, EventArgs args)
+        {
+            if (string.IsNullOrWhiteSpace(this.TxtName.Text))
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok,
+                    "Ingrese un nombre");
+                ms.Run();
+                ms.Destroy();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(this.TxtEstado.Text))
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok,
+                    "Ingrese un estado");
+                ms.Run();
+                ms.Destroy();
+                return;
+            }
+            try
+            {
+                this.DtRol.InsertInto(
+                    this.TxtName.Text,
+                    this.TxtEstado.Text
+                    );
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok,
+                    "Rol agregado");
+                ms.Run();
+                ms.Destroy();
+            }
+            catch (Exception e)
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Error, ButtonsType.Ok,
+                    e.Message);
+                ms.Run();
+                ms.Destroy();
+            }
+            this.UpdateData();
+        }
+
+        protected void BtnEditOnClicked(object sender, EventArgs args)
+        {
+            if (this.SelectedID < 0)
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Warning, ButtonsType.Ok,
+                    "Seleccione un rol en la tabla");
+                ms.Run();
+                ms.Destroy();
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(this.TxtName.Text) ||
+                string.IsNullOrWhiteSpace(this.TxtEstado.Text))
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok,
+                    "No puede haber datos vacíos");
+                ms.Run();
+                ms.Destroy();
+                return;
+            }
+
+            try
+            {
+                this.DtRol.UpdateSet(
+                    this.SelectedID.ToString(),
+                    this.TxtName.Text, this.TxtEstado.Text
+                    );
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok, "Rol editado");
+                ms.Run();
+                ms.Destroy();
+            }
+            catch (Exception e)
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Error, ButtonsType.Ok, e.Message);
+                ms.Run();
+                ms.Destroy();
+            }
+            this.UpdateData();
+        }
+
+        protected void BtnRemoveOnClicked(object sender, EventArgs args)
+        {
+            if (this.SelectedID < 0)
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Warning, ButtonsType.Ok,
+                    "Seleccione un cargo en la tabla");
+                ms.Run();
+                ms.Destroy();
+                return;
+            }
+
+            string rolName = this.GetCargoValue(this.SelectedID, 1)?.ToString() ?? "";
+
+            MessageDialog deletePrompt = new MessageDialog(this, DialogFlags.Modal,
+                MessageType.Question, ButtonsType.YesNo,
+                $"¿Desea eliminar el rol \"{rolName}\" ({this.SelectedID})?");
+            int result = deletePrompt.Run();
+            deletePrompt.Destroy();
+
+            switch (result)
+            {
+                case (int)ResponseType.Yes:
+                    break;
+                default:
+                    return;
+            }
+
+            try
+            {
+                this.DtRol.DeleteFrom(this.SelectedID.ToString());
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Info, ButtonsType.Ok,
+                    "Rol eliminado");
+                ms.Run();
+                ms.Destroy();
+            }
+            catch (Exception e)
+            {
+                MessageDialog ms = new MessageDialog(this, DialogFlags.Modal,
+                    MessageType.Error, ButtonsType.Ok,
+                    e.Message);
+                ms.Run();
+                ms.Destroy();
+            }
+            this.SelectedID = -1;
+            this.UpdateData();
+        }
+
+        protected object GetCargoValue(int idCargo, int column)
+        {
+            TreeIter iter;
+            TreeModel model = this.TreeData;
+
+            object value = null;
+
+            if (model.GetIterFirst(out iter))
+            {
+                do
+                {
+                    if (idCargo.ToString() == model.GetValue(iter, 0).ToString())
+                    {
+                        value = model.GetValue(iter, column);
+                    }
+                } while (model.IterNext(ref iter));
+            }
+
+            return value;
+        }
+
+        protected void ViewTableOnRowActivated(object o, RowActivatedArgs args)
+        {
+            TreeSelection selection = this.viewTable.Selection;
+            TreeIter iter;
+            TreeModel model;
+
+            if (selection.GetSelected(out model, out iter))
+            {
+                string selectedID = model.GetValue(iter, 0).ToString();
+                try
+                {
+                    this.SelectedID = Int32.Parse(selectedID);
+                    this.SetEntryTextFromID(this.SelectedID);
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+            }
+        }
+
+        protected void SetEntryTextFromID(int id)
+        {
+            TreeIter iter;
+            if (this.TreeData.GetIterFirst(out iter))
+            {
+                do
+                {
+                    if (id.ToString() == (string)this.TreeData.GetValue(iter, 0))
+                    {
+                        this.TxtName.Text = (string)TreeData.GetValue(iter, 1);
+                        this.TxtEstado.Text = (string)TreeData.GetValue(iter, 2);
+                        return;
+                    }
+                    else
+                    {
+                        this.TxtName.Text = "";
+                        this.TxtEstado.Text = "";
+                    }
+                }
+                while (TreeData.IterNext(ref iter));
+            }
+        }
+
+        protected bool ViewTableEqualFunc(TreeModel model, int column, string key, TreeIter iter)
+        {
+            for (int i = 0; i < model.NColumns; i++)
+            {
+                string value = (string)model.GetValue(iter, i);
+                if (value.ToLower().Contains(key.ToLower()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        protected bool TreeModelFilterVisible(TreeModel model, TreeIter iter)
+        {
+            TreePath path = model.GetPath(iter);
+            //Console.WriteLine($"'{this.TxtSearch.Text}' at '{path.ToString()}'");
+            if (string.IsNullOrWhiteSpace(this.TxtSearch.Text))
+            {
+                //Console.WriteLine("Empty search");
+                return true;
+            }
+            for (int i = 0; i < model.NColumns; i++)
+            {
+                string value = (string)model.GetValue(iter, i);
+                //Console.WriteLine($"\t{i}: '{value}'");
+                if (value.ToLower().Contains(this.TxtSearch.Text.ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected void TxtSearchOnChanged(object sender, EventArgs e)
+        {
+            //Console.WriteLine($"Changed: '{this.TxtSearch.Text}'");
+            this.TreeData.Refilter();
+        }
+    }
+}
---
+++ SistemaEyS/Database/DatosSeguridad/Dt_tbl_rol.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data;
+using Gtk;
+using System.Text;
+using SistemaEyS.Database.Connection;
+
+namespace SistemaEyS.DatosSeguridad
+{
+    public class Dt_tbl_rol : DataTableTemplate
+    {
+        public Dt_tbl_rol()
+        {
+            this.conn = ConnectionSeg.OpenConnection();
+            this.DBTable = "Seguridad.tbl_rol";
+            this.Model = new ListStore(
+                typeof(string),
+                typeof(string),
+                typeof(string)
+                );
+        }
+        public override void UpdateModel()
+        {
+            this.Model.Clear();
+
+            IDataReader idr = null;
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            sb.Append("SELECT * FROM Seguridad.tbl_rol;");
+            try
+            {
+                idr = conn.Read(CommandType.Text, sb.ToString());
+                while (idr.Read())
+                {
+                    this.Model.AppendValues(
+                        idr[0].ToString(), // ID Rol
+                        idr[1].ToString(), // Nombre
+                        idr[2].ToString() // Estado
+                        );
+                }
+            }
+            catch (Exception e)
+            {
+                MessageDialog ms = new MessageDialog(null, DialogFlags.Modal,
+                    MessageType.Error, ButtonsType.Ok, e.Message);
+                ms.SetPosition(WindowPosition.Mouse);
+                ms.Run();
+                ms.Destroy();
+            }
+            finally
+            {
+                if (idr != null && !idr.IsClosed)
+                {
+                    idr.Close();
+                }
+            }
+        }
+
+        public void InsertInto(string rol, string estado)
+        {
+            this.InsertInto(
+                    //this.conn,
+                    new DataTableParameter("rol", $"'{rol}'"),
+                    new DataTableParameter("estado", $"'{estado}'")
+                );
+        }
+
+        public void UpdateSet(string id_rol, string rol, string estado)
+        {
+            Console.WriteLine(id_rol);
+            this.UpdateSet(
+                    //this.conn,
+                    new DataTableParameter("id_rol", id_rol),
+                    new DataTableParameter(
+                        !string.IsNullOrWhiteSpace(rol) ? "rol" : "",
+                        $"'{rol}'"
+                        ),
+                    new DataTableParameter(
+                        !string.IsNullOrWhiteSpace(estado) ? "estado" : "",
+                        $"'{estado}'"
+                        )
+                );
+        }
+
+        public void DeleteFrom(string id_rol)
+        {
+            this.DeleteFrom(this.conn,
+                new DataTableParameter("id_rol", id_rol)
+                );
+        }
+        public void DeleteFromUpdate(string id_rol)
+        {
+            this.UpdateSet(
+                new DataTableParameter("id_rol", id_rol),
+                new DataTableParameter("estado", "3")
+                );
+        }
+    }
+}
