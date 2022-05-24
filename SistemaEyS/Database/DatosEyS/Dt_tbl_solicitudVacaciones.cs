3d0b5e6ca0fd239b5dda18161ed307ac4ef73e95
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-24 01:40:13 -0600

    Change the conexion with database to JezerStyle

--- SistemaEyS/Database/DatosEyS/Dt_tbl_solicitudVacaciones.cs
+++ SistemaEyS/Database/DatosEyS/Dt_tbl_solicitudVacaciones.cs
@@ -3,37 +3,44 @@ using System.Data;
 using Gtk;
 using System.Text;
 using SistemaEyS.Database.Connection;
+using SistemaEyS.DatosEyS;
 
 namespace SistemaEyS.DatosEyS
 {
-    public class Dt_tlb_solicitudVacaciones
+    public class Dt_tlb_SolVacaciones : DataTableTemplate
     {
 
-        public Gtk.ListStore listStore;
+        public ListStore ModelView;
 
-        ConnectionEyS conn = ConnectionEyS.OpenConnection();
-        StringBuilder sb = new StringBuilder();
-
-        public ListStore listarSolicitudVacaciones()
+        public Dt_tlb_SolVacaciones()
         {
-            ListStore datos = new ListStore(
-                typeof(string), typeof(string),
-                typeof(string), typeof(string),
+            this.conn = ConnectionEyS.OpenConnection();
+            this.DBTable = "BDSistemaEyS.SolVacaciones";
+            this.ModelView = new ListStore(
+                typeof(string), typeof(string), typeof(string),
+                typeof(string), typeof(string), typeof(string)
+            );
+            this.Model = new ListStore(
+                typeof(string), typeof(string), typeof(string),
+                typeof(string), typeof(string), typeof(string),
                 typeof(string), typeof(string)
             );
+        }
 
-            Console.WriteLine("UWU");
-
+        public void UpdateModelEmp()
+        {
+            this.Model.Clear();
             IDataReader idr = null;
+            StringBuilder sb = new StringBuilder();
             sb.Clear();
             sb.Append("SELECT * FROM BDSistemaEyS.SolVacaciones;");
             try
             {
-                idr = conn.Read(CommandType.Text, sb.ToString());
+                idr = this.conn.Read(CommandType.Text, sb.ToString());
 
                 while (idr.Read())
                 {
-                    datos.AppendValues(
+                    this.Model.AppendValues(
                         idr[0].ToString(), // ID
                         idr[3].ToString(), // ID Empleado
                         idr[1].ToString(), // Fecha de Solicitud
@@ -42,12 +49,12 @@ namespace SistemaEyS.DatosEyS
                         idr[5].ToString() // Fin
                     );
                 }
-                return datos;
             }
             catch (Exception e)
             {
                 MessageDialog ms = new MessageDialog(null, DialogFlags.Modal, MessageType.Error,
                     ButtonsType.Ok, e.Message);
+                Console.WriteLine(e);
                 ms.Run();
                 ms.Destroy();
             }
@@ -58,10 +65,71 @@ namespace SistemaEyS.DatosEyS
                     idr.Close();
                 }
             }
-            return datos;
         }
-        public Dt_tlb_solicitudVacaciones()
+
+
+        public override void UpdateModel()
         {
+            this.UpdateModelEmp();
+            //this.UpdateModelView();
         }
+
+        public ListStore GetDataView()
+        {
+            this.UpdateModel();
+            return this.ModelView;
+        }
+
+        public void InsertInto(string fechaSol, string descripcionSol, string idEmpleado, string FechaHoraInicio,
+            string FechaHoraFin 
+            )
+        {
+            this.InsertInto(
+                new DataTableParameter("fechaSol", $"'{fechaSol}'"),
+                new DataTableParameter("descripcionSol", $"'{descripcionSol}'"),
+                new DataTableParameter("idEmpleado", $"'{idEmpleado}'"),
+                new DataTableParameter("FechaHoraInicio", $"'{FechaHoraInicio}'"),
+                new DataTableParameter("FechaHoraFin", $"'{FechaHoraFin}'")
+                );
+        }
+
+
+        public void UpdateSet(string idSolVacaciones, string fechaSol, string descripcionSol, string idEmpleado, string FechaHoraInicio,
+                                string FechaHoraFin)
+
+        {
+            this.UpdateSet(
+                new DataTableParameter("idSolVacaciones", $"'{idSolVacaciones}'"),
+                new DataTableParameter(
+                    !string.IsNullOrWhiteSpace(fechaSol) ? "fechaSol" : "",
+                    $"'{fechaSol}'"
+                    ),
+                new DataTableParameter(
+                    !string.IsNullOrWhiteSpace(descripcionSol) ? "descripcionSol" : "",
+                    $"'{descripcionSol}'"
+                    ),
+                new DataTableParameter(
+                    !string.IsNullOrWhiteSpace(idEmpleado) ? "idEmpleado" : "",
+                    $"'{idEmpleado}'"
+                    ),
+                new DataTableParameter(
+                    !string.IsNullOrWhiteSpace(FechaHoraInicio) ? "FechaHoraInicio" : "",
+                    $"'{FechaHoraInicio}'"
+                    ),
+                new DataTableParameter(
+                    !string.IsNullOrWhiteSpace(FechaHoraFin) ? "FechaHoraFin" : "",
+                    $"'{FechaHoraFin}'"
+                    )
+
+                );
+        }
+
+        public void DeleteFrom(string idSolVacaciones)
+        {
+            this.DeleteFrom(this.conn,
+                new DataTableParameter("idSolVacaciones", $"'{idSolVacaciones}'")
+                );
+        }
+
     }
 }
