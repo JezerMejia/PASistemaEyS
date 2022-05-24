35fc576f5f87564a7a860cb2a3a888ab45326482
Jezer Mejía <amyuki4@icloud.com>
2022-05-23 22:28:19 -0600

    Added Entidades and Negocio layers for Seguridad.tbl_user

---
+++ SistemaEyS/Database/DatosSeguridad/Entidades/Ent_user.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using SistemaEyS.DatosSeguridad.Datos;
+
+public enum EntidadEstado
+{
+    Indefinido = 0,
+    Añadido = 1,
+    Modificado = 2,
+    Eliminado = 3
+}
+
+namespace SistemaEyS.DatosSeguridad.Entidades
+{
+    public class Ent_user
+    {
+        protected Dt_tbl_user DtUser = new Dt_tbl_user();
+
+        private int _id_user;
+        private string _user;
+        private string _pwd;
+        private string _nombres;
+        private string _apellidos;
+        private string _email;
+        private int _estado;
+        private string _pwd_temp = null;
+
+        public int id_user {
+            get => this._id_user;
+            set => this._id_user = value;
+	    }
+        public string user
+        {
+            get => this._user;
+            set => this._user = value;
+        }
+        public string pwd
+        {
+            get => this._pwd;
+            set => this._pwd = value.Substring(
+                0, value.Length > 50 ? 50 : value.Length
+		        );
+        }
+        public string nombres
+        {
+            get => this._nombres;
+            set => this._nombres = value.Substring(
+                0, value.Length > 50 ? 50 : value.Length
+		        );
+        }
+        public string apellidos
+        {
+            get => this._apellidos;
+            set => this._apellidos = value.Substring(
+                0, value.Length > 50 ? 50 : value.Length
+		        );
+        }
+        public string email
+        {
+            get => this._email;
+            set
+            {
+                if (!Regex.IsMatch(
+                    value,
+                    @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$"
+                    )
+                )
+                {
+                    throw new FormatException("El correo no es válido");
+                };
+                this._email = value;
+            }
+        }
+        public EntidadEstado estado
+        {
+            get => (EntidadEstado)this._estado;
+            set => this._estado = (int)value;
+        }
+        public string pwd_temp
+        {
+            get => this._pwd_temp;
+            set => this._pwd_temp = value.Substring(
+                0, value.Length > 50 ? 50 : value.Length
+		        );
+        }
+
+
+        public Ent_user()
+        {
+        }
+    }
+}
---
+++ SistemaEyS/Database/DatosSeguridad/Negocio/Neg_user.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using SistemaEyS.DatosSeguridad.Datos;
+using SistemaEyS.DatosSeguridad.Entidades;
+using Gtk;
+
+namespace SistemaEyS.DatosSeguridad.Negocio
+{
+    public class Neg_user
+    {
+        protected Dt_tbl_user DtUser = new Dt_tbl_user();
+
+        public Neg_user()
+        {
+        }
+
+        public void ValidateUser(Ent_user user)
+        {
+            if (this.DtUser.DoesExist(
+                new DataTableParameter("user", $"{user.user}")
+            ))
+            {
+                throw new Exception("El usuario ya existe");
+            }
+        }
+        public void ValidateEmail(Ent_user user)
+        {
+            if (this.DtUser.DoesExist(
+                new DataTableParameter("email", $"{user.email}")
+            ))
+            {
+                throw new Exception("El correo ya existe");
+            }
+        }
+
+        public void AddUser(Ent_user user)
+        {
+            try
+            {
+                this.ValidateUser(user);
+                this.ValidateEmail(user);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e;
+            }
+            this.DtUser.InsertInto(
+                user.user, user.pwd,
+                user.nombres, user.apellidos,
+                user.email,
+                ((int)user.estado).ToString()
+            );
+        }
+        public void EditUser(Ent_user user)
+        {
+            try
+            {
+                Ent_user prevUser = this.SearchUser(user.id_user);
+
+                if (prevUser.user != user.user)
+                    this.ValidateUser(user);
+                if (prevUser.email != user.email)
+                    this.ValidateEmail(user);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e;
+            }
+            this.DtUser.UpdateSet(
+                user.id_user.ToString(), user.user, user.pwd,
+                user.nombres, user.apellidos, user.email,
+                ((int)user.estado).ToString()
+            );
+        }
+        public void RemoveUser(Ent_user user)
+        {
+            this.DtUser.DeleteFromUpdate(user.id_user.ToString());
+        }
+        public Ent_user SearchUser(int id_user)
+        {
+            ListStore store = this.DtUser.Search(
+                new DataTableParameter("id_user", $"{id_user}")
+            );
+            if (store == null) throw new NullReferenceException("El usuario no existe");
+            TreeIter iter;
+
+            if (!store.GetIterFirst(out iter)) throw new NullReferenceException("No hay datos del usuario");
+
+            Ent_user user = new Ent_user()
+            {
+                id_user = Int32.Parse(store.GetValue(iter, 0).ToString()),
+                user = store.GetValue(iter, 1).ToString(),
+                pwd = store.GetValue(iter, 2).ToString(),
+                nombres = store.GetValue(iter, 3).ToString(),
+                apellidos = store.GetValue(iter, 4).ToString(),
+                email = store.GetValue(iter, 5).ToString(),
+                pwd_temp = store.GetValue(iter, 6).ToString(),
+                estado = (EntidadEstado)Int32.Parse(store.GetValue(iter, 7).ToString()),
+            };
+
+            return user;
+        }
+    }
+}
