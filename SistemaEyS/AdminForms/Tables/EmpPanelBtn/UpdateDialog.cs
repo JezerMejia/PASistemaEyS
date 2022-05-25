b5fd5686bbdf996c2961b05aca315c584e010b4d
Jezer Mejía <amyuki4@icloud.com>
2022-05-24 20:00:19 -0600

    Validate dIngress text as a Date

--- SistemaEyS/AdminForms/Tables/EmpPanelBtn/UpdateDialog.cs
+++ SistemaEyS/AdminForms/Tables/EmpPanelBtn/UpdateDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using Gtk;
 using SistemaEyS.DatosEyS.Datos;
 using SistemaEyS.DatosEyS.Entidades;
@@ -167,6 +168,10 @@ namespace SistemaEyS.AdminForms.Tables.EmpPanelBtn
                 DateTime? fechaIngreso = null;
                 if (!string.IsNullOrWhiteSpace(this.dIngress.Text))
                 {
+                    if (!Regex.IsMatch(this.dIngress.Text,
+                        @"^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])$")
+                    )
+                        throw new FormatException("Fecha de ingreso inválida");
                     fechaIngreso = DateTime.Parse(this.dIngress.Text);
                 }
                 int? idHor = null;
