28dd2552c5abd7e4afe32782980d3c7b0e7321ae
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-25 03:40:35 -0600

    In working

--- SistemaEyS/Database/DatosEyS/Negocio/Neg_Cargo.cs
+++ SistemaEyS/Database/DatosEyS/Negocio/Neg_Cargo.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-namespace SistemaEyS.Database.DatosEyS.Negocio
+using SistemaEyS.DatosEyS.Datos;
+using SistemaEyS.DatosEyS.Entidades;
+using Gtk;
+
+namespace SistemaEyS.DatosEyS.Negocio
 {
     public class Neg_Cargo
     {
+
+        protected Dt_tbl_cargo DtCar = new Dt_tbl_cargo();
+
         public Neg_Cargo()
         {
+        }
+
+        public void ValidateID(Ent_Cargo car)
+        {
+            if (this.DtCar.DoesExist(
+                new DataTableParameter("idEmpleado", $"{car.idCargo}")
+            ))
+            {
+                throw new Exception("El cargo ya existe");
+            }
         }
     }
 }
