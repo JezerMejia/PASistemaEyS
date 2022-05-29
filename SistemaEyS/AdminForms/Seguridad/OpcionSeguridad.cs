8856855caa42af9e3291a32223dc72728d3bc57b
KaiserStuka <leocnavarrete3@gmail.com>
2022-05-29 01:32:42 -0600

    Added a interface in OpcionSeguridad

---
+++ SistemaEyS/AdminForms/Seguridad/OpcionSeguridad.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Gtk;
+using SistemaEyS.DatosSeguridad.Datos;
+
+namespace SistemaEyS.AdminForms.Seguridad
+{
+    public partial class OpcionSeguridad : Gtk.Window
+    {
+
+        protected Dt_tbl_opcion DtOpc = new Dt_tbl_opcion();
+
+        protected TreeModelFilter TreeData;
+        protected TreeModelFilterVisibleFunc ModelFilterFunc;
+        public int SelectedID = -1;
+
+        public OpcionSeguridad() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+            this.Hide();
+
+            this.ModelFilterFunc = new TreeModelFilterVisibleFunc(this.TreeModelFilterVisible);
+
+            this.viewTable.SearchEntry = this.TxtSearch;
+            this.viewTable.SearchEqualFunc = new TreeViewSearchEqualFunc(this.ViewTableEqualFunc);
+
+            this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
+            {
+                args.RetVal = this.HideOnDelete();
+            };
+
+            StoreObject[] storeObjects = {
+                new StoreObject("ID", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Opcion", typeof(string), "text", new Gtk.CellRendererText()),
+                new StoreObject("Descripcion", typeof(string), "text", new Gtk.CellRendererText()),
+            };
+            this.viewTable.SetTreeViewColumns(storeObjects);
+
+            this.UpdateData();
+        }
+
+        public void UpdateData()
+        {
+            this.TreeData = new TreeModelFilter(DtOpc.GetData(), null);
+            this.TreeData.VisibleFunc = this.ModelFilterFunc;
+            this.viewTable.Model = this.TreeData;
+
+        }
+
+        protected bool TreeModelFilterVisible(TreeModel model, TreeIter iter)
+        {
+            if (string.IsNullOrWhiteSpace(this.TxtSearch.Text))
+            {
+                return true;
+            }
+            for (int i = 0; i < model.NColumns; i++)
+            {
+                string value = (string)model.GetValue(iter, i);
+                if (string.IsNullOrEmpty(value)) return false;
+                if (value.ToLower().Contains(this.TxtSearch.Text.ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected bool ViewTableEqualFunc(TreeModel model, int column, string key, TreeIter iter)
+        {
+            for (int i = 0; i < model.NColumns; i++)
+            {
+                string value = (string)model.GetValue(iter, i);
+                if (string.IsNullOrWhiteSpace(value)) return true;
+                if (value.ToLower().Contains(key.ToLower()))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
