08e47ed8cf644d34d4b66953fc9e9c6c4a97676c
Jezer Mejía <amyuki4@icloud.com>
2022-05-29 22:00:26 -0600

    Update Data on Add/Edit Horario

--- SistemaEyS/AdminForms/Tables/HorPanelBtn/AddDialogHor.cs
+++ SistemaEyS/AdminForms/Tables/HorPanelBtn/AddDialogHor.cs
@@ -11,11 +11,14 @@ namespace SistemaEyS.AdminForms.Tables.HorPanelBtn
         protected Neg_Horario NegHor = new Neg_Horario();
         public int SelectedID = -1;
 
-        public AddDialogHor() :
+        protected HorarioView parent;
+
+        public AddDialogHor(HorarioView parent) :
                 base(Gtk.WindowType.Toplevel)
         {
             this.Build();
             this.Hide();
+            this.parent = parent;
             this.DeleteEvent += delegate (object obj, DeleteEventArgs args)
             {
                 args.RetVal = this.HideOnDelete();
@@ -77,6 +80,8 @@ namespace SistemaEyS.AdminForms.Tables.HorPanelBtn
                 };
 
                 this.NegHor.AddHorario(hor);
+
+                this.parent.UpdateData();
 
                 this.mensaje("Guardado");
                 this.ClearInput();
